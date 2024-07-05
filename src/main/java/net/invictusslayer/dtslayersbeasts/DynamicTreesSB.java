cfa017d832cc084c173e4900cd7eac0944c807ab
InvictusSlayer <ojsutters@gmail.com>
2024-07-05 15:31:46 +0100

    v0.1.1 removed redundant resources as data gen includes mushrooms updated dt & dt+ versions

--- src/main/java/net/invictusslayer/dtslayersbeasts/DynamicTreesSB.java
+++ src/main/java/net/invictusslayer/dtslayersbeasts/DynamicTreesSB.java
@@ -42,9 +42,6 @@ public class DynamicTreesSB {
 	private void clientSetup(FMLClientSetupEvent event) {}
 
 	private void gatherData(GatherDataEvent event) {
-		GatherDataHelper.gatherTagData(MOD_ID, event);
-		GatherDataHelper.gatherLootData(MOD_ID, event);
-		GatherDataHelper.gatherBlockStateAndModelData(MOD_ID, event, SoilProperties.REGISTRY, Family.REGISTRY, Species.REGISTRY, LeavesProperties.REGISTRY, CapProperties.REGISTRY);
-		GatherDataHelper.gatherItemModelData(MOD_ID, event, SoilProperties.REGISTRY, Family.REGISTRY, Species.REGISTRY, LeavesProperties.REGISTRY, CapProperties.REGISTRY);
+		GatherDataHelper.gatherAllData(MOD_ID, event, SoilProperties.REGISTRY, Family.REGISTRY, Species.REGISTRY, LeavesProperties.REGISTRY, CapProperties.REGISTRY);
 	}
 }
